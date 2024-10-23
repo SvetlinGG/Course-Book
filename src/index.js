26de32ef2c5734696969e19d6413126602eac8c5
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-10-24 00:31:42 +0200

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import express from 'express';
+const express = require('express');
+
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Just works!');
-});
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+app.use(routes);
+
 
 app.listen(1000, () => console.log('Server is listen on http://localhost:1000'));
