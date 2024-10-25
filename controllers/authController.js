b4da649342cc2342333fe16d9bc3e2d9dc82fa51
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-10-25 15:45:32 +0200

    Add error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
-const authService = require('../services/authService.js')
+const authService = require('../services/authService.js');
+const { getErrorMessage } = require('../utils/errorUtils.js');
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
@@ -8,11 +9,19 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
     const userData = req.body;
 
+    try{
+
     const token = await authService.register(userData);
 
     res.cookie('auth', token);
 
     res.redirect('/');
+
+    }catch(err){
+        res.render('auth/register', {error: getErrorMessage(err)});
+    }
+
+    
 });
 
 router.get('/login', (req, res) => {
@@ -21,6 +30,17 @@ router.get('/login', (req, res) => {
 
 router.post('/login', async (req, res) => {
     const loginData = req.body;
+
+    try {
+
+    const token = await authService.login(loginData);
+
+    res.cookie('auth', token);
+    res.redirect('/');
+        
+    } catch (err) {
+        res.render('/auth/login', {error: getErrorMessage(err)});
+    }
 
     const token = await authService.login(loginData);
 
