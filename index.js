dc2fd7654f1c9cb8ed85a965adb6dccc4ebabef4
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-10-25 02:04:07 +0200

    add middleware

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
 
 const routes = require('./routes');
+const {authMiddleware} = require('./middlewares/authMiddleware');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(authMiddleware);
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
 app.set('view engine', 'hbs');
+
 
 
 app.use(routes);
